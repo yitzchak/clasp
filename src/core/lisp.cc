42045eb8f3551f54bb11978d91ad9c12237790f0
drmeister <chris.schaf@verizon.net>
2018-06-27 11:17:59 -0400

    more fdesc changes

--- src/core/lisp.cc
+++ src/core/lisp.cc
@@ -569,6 +569,8 @@ void Lisp_O::startupLispEnvironment(Bundle *bundle) {
   printf("%s:%d startupLispEnvironment initialize everything\n", __FILE__, __LINE__ );
 #endif
   this->_Roots._CommandLineArguments = _Nil<T_O>();
+  mp::Process_sp main_process = mp::Process_O::make_process(INTERN_(core,top_level),_Nil<T_O>(),_lisp->copy_default_special_bindings(),_Nil<T_O>(),0);
+  my_thread->initialize_thread(main_process);
   {
     _BLOCK_TRACE("Initialize other code"); // needs _TrueObject
     initialize_Lisp_O();
@@ -652,8 +654,6 @@ void Lisp_O::startupLispEnvironment(Bundle *bundle) {
   //
   // Initialize the main thread info
   //
-  mp::Process_sp main_process = mp::Process_O::make_process(INTERN_(core,top_level),_Nil<T_O>(),_lisp->copy_default_special_bindings(),_Nil<T_O>(),0);
-  my_thread->initialize_thread(main_process);
   {
     // initialize caches
     my_thread->_SingleDispatchMethodCachePtr = gc::GC<Cache_O>::allocate();
